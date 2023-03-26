6f303bf54ab694f076b8e1b2216515da693c8acc
shm_usuf <shmusuf@gmail.com>
2023-03-26 22:31:06 +0600

    next routing added

--- pages/products/index.js
+++ pages/products/index.js
@@ -7,7 +7,6 @@ const Products = ({ productId = 10 }) => {
       <Link href="/products/1">Product 1</Link>
       <Link href="/products/2">Product 2</Link>
       <Link href="/products/3">Product 3</Link>
-      <Link href="/products/4">Product 4</Link>
       <Link href={`/products/${productId}`} replace>
         Product {productId}
       </Link>
