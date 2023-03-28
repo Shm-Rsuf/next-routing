90d54c593b721fcacaa596195986368b85b3f14b
shm_usuf <shmusuf@gmail.com>
2023-03-28 12:34:48 +0600

    get Static Paths

--- pages/posts/[postId].js
+++ pages/posts/[postId].js
@@ -11,6 +11,22 @@ const PostDetails = ({ post }) => {
 
 export default PostDetails;
 
+export async function getStaticPaths() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const data = await res.json();
+
+  const paths = data.map((post) => {
+    return {
+      params: { postId: `${post.id}` },
+    };
+  });
+
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
 export async function getStaticProps(context) {
   const { params } = context;
 
